bc19f0daed543e65b496dcbc8a7907b80c86d90f
Taiyo Maruyama <stickytomatobaby0501@gmail.com>
2023-05-19 15:45:37 +0900

    add/next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,17 @@ import "@/styles/userInfo.css";
 import "@/styles/todoList.css";
 import "@/styles/create.css";
 import "@/styles/edit.css";
+import { SessionProvider } from "next-auth/react";
 
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
